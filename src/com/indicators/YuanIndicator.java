9f3be7e093a106f7160a5cfb9041c1f35316cf14
WEX <wex@wexs-mbp.hel.fi.ibm.com>
2018-11-08 15:53:08 +0200

    yuan indicator added

---
+++ src/com/indicators/YuanIndicator.java
@@ -0,0 +1,203 @@
+package com.indicators;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+/*
+ * 5.3 million yuan
+ */
+public class YuanIndicator implements Indicator {
+
+	// PUT this true to enable sytem out debugs...
+	public static final boolean DEBUG = true;
+
+	public static final String MILLION = "million";
+	public static final String BILLION = "billion";
+	public static final String TRILLION = "trillion";
+	public static final String MONEY = "money";
+
+	private String className = "";
+	private String tag = "";
+
+	private Set<String> millionPostNames = new HashSet<String>();
+	private Set<String> billionPostNames = new HashSet<String>();
+	private Set<String> trillionPostNames = new HashSet<String>();
+
+	private List<String> okCharacters = new ArrayList<String>();
+
+	public YuanIndicator(String classNameIN,String tagIN) {
+		this.className = classNameIN;
+		this.tag = tagIN;
+
+		millionPostNames.add(" million yuan");
+		billionPostNames.add(" billion yuan");
+		trillionPostNames.add(" trillion yuan");
+
+		// These are characters that we allow as 'numbers', so these are digits
+		// plus . and , comma being silent.
+
+		okCharacters.add("0");
+		okCharacters.add("1");
+		okCharacters.add("2");
+		okCharacters.add("3");
+		okCharacters.add("4");
+		okCharacters.add("5");
+		okCharacters.add("6");
+		okCharacters.add("7");
+		okCharacters.add("8");
+		okCharacters.add("9");
+		okCharacters.add("0");
+		okCharacters.add(",");
+		okCharacters.add(".");
+
+	}
+
+	@Override
+	public Collection<Annotation> identify(String text, int offset) {
+		Collection<Annotation> matches = new HashSet<Annotation>();
+
+		if(text == null || "".equals(text.trim()))
+			return matches;
+
+		// in order not to mess with the white spaces, we should not trim the actual string
+		String lowerText = text.toLowerCase();
+
+		if(DEBUG)
+			System.out.println("before million");
+		// millions
+		findPostMatches(lowerText, offset, this.MILLION, millionPostNames,matches);
+
+		if(DEBUG)
+			System.out.println("before billion");
+		// billions
+		findPostMatches(lowerText, offset,this.BILLION, billionPostNames,matches);
+
+		if(DEBUG)
+			System.out.println("before trillion");
+		// trillions
+		findPostMatches(lowerText, offset,this.TRILLION, trillionPostNames,matches);
+
+		return matches;
+	}
+	
+	public void findPostMatches(String text, int offset, String matchType, Set<String>postNames, 
+			Collection<Annotation> matches) {
+		int currentIndex = 0;
+		Iterator<String> iterator = postNames.iterator();
+
+		// here we loop through all possible post matches
+		while(iterator.hasNext()) {
+			String postString = iterator.next();
+			currentIndex = 0;
+
+			// as long we find possible matches...
+			int matchIndex = text.indexOf(postString,currentIndex);
+			while(matchIndex > 0 ) {
+
+				System.out.println("postString="+postString + " currentIndex="+currentIndex);
+				// We have a possible match
+				isPostMatch(text, offset, matchIndex,postString,matchType, matches);
+
+				// lets go forward to the last match + 1
+				currentIndex = matchIndex+1;
+				matchIndex = text.indexOf(postString,currentIndex);	
+			}
+
+		}
+
+	}
+
+	// Potential match, lets determine do we have a value
+	// We need to look backwards in post match determination
+	public void isPostMatch(String text, int offset, int matchIndex, String postString, String matchType,
+			Collection<Annotation> matches) {
+
+		if(DEBUG) 
+			System.out.println("We are at isPostMatch:"+text+" offset:"+offset+ " matchIndex="+matchIndex+ " postString="+postString+
+					" matches.size()="+matches.size());
+		int numbersInFront = findNumbersInFront(text,matchIndex);
+		if(DEBUG) 
+			System.out.println("numbers in front="+numbersInFront);
+		if(numbersInFront <= 0)
+			return;
+
+		String valueString = text.substring(matchIndex-numbersInFront, matchIndex);
+
+		String fullString = valueString+postString;
+
+		// checking for "US " in front
+		int usCorrection = 0;
+		//if((matchIndex-numbersInFront-3) >= 0)
+		//	if("us ".equals(text.substring(matchIndex-numbersInFront-3, matchIndex-numbersInFront))) {
+		//		fullString="us "+fullString;
+		//		usCorrection = -3;
+		//	}	
+
+		// we likely will not have these - at least there were no examples like 1,234 million yuan in the text
+		String valueWithoutCommas = valueString.replaceAll(",",""); 
+
+		// TODO DEBUG
+		if(DEBUG) {
+			System.out.println("fullString="+fullString);
+			System.out.println("valueString="+valueString);
+			// This is for making sure that 1,000 is seen as 1000
+
+			System.out.println("valueWithoutCommas="+valueWithoutCommas);
+			System.out.println("");
+		}
+
+		Annotation annotation = new Annotation();
+		annotation.className = this.className+matchType; // We assume that we can just post append
+		annotation.value = valueWithoutCommas;
+		annotation.start = offset + usCorrection + (matchIndex-numbersInFront);
+		annotation.stop = offset + (matchIndex+postString.length());
+		annotation.tags.add(matchType);
+		annotation.tags.add(MONEY);
+		annotation.fullString = fullString;
+		matches.add(annotation);
+
+	}
+
+	public int findNumbersInFront(String text, int matchIndex)  {
+		int matches = 0;
+
+		int counter = -1;
+		// Let's not go negative ever...
+		boolean match = true;
+		while(matchIndex +counter >= 0 && match) {
+			String character = text.substring(matchIndex+counter, matchIndex+counter+1); 
+			if(DEBUG)
+				System.out.println("Testing character=("+character+")");
+			// lets check do we have a hit
+			match = false;
+			for(int i = 0 ; i < okCharacters.size() && !match; i ++) {
+				if(okCharacters.get(i).equals(character))
+					match = true;
+			}
+
+			// only in case we have previous matching character... we increase the matching
+			if(match)  {
+				counter-=1;
+				matches++;
+			}
+		}
+
+		return matches;
+
+	}
+
+	@Override
+	public Collection<Annotation> identify(String text, int offset, Indicator condition) {
+		Collection<Annotation> matches = condition.identify(text,offset);
+
+		if(matches.size()>0)
+			matches = this.identify(text,offset);
+
+		return matches;
+	}
+
+}
--- test/com/indicators/YuanIndicatorTest.java
+++ test/com/indicators/YuanIndicatorTest.java
@@ -9,159 +9,92 @@ import org.junit.jupiter.api.Test;
 class YuanIndicatorTest {
 
 	public static final String NORMAL_SENTENCE = "This is a normal, ordinary sentence.";
-	public static final String START_OF_SENTENCE = "The investing deal size was ";
+	public static final String START_OF_SENTENCE = "The investing deal size was";
 	public static final String END_OF_SENTENCE = " as evaluated in end of 2017 third quarter.";
 
-	public static final String RMBA = "88.14 billion yuan";
-	public static final String RMBB = "13 billion yuan";
-	public static final String RMBC = "RM55 billion";
-	public static final String RMBD = "RM21.9 billion";
-	public static final String RMBE = "RM141bil";
-	public static final String RMBF = "RM141 billion";
-	public static final String RMBG = "RMB 1.02billion";
-	public static final String RMBH = "RM141b";
+	public static final String MONTHA = "1,075 billion yuan";
+	public static final String MONTHA_VALUE = "1075";
+	public static final String MONTHB = "19.6 million yuan";
+	public static final String MONTHB_VALUE = "19.6";
+	public static final String MONTHC = "2 trillion yuan";
+	public static final String MONTHC_VALUE = "2";
 
-	public static final String RMBI = "RMB7.4 trillion";
-
-	public static final String RMBJ = "RM20.70 million";
-	public static final String RMBK = "RMB 208.6million";
-
-	public static final String RMBL = "RM62,736";
-	public static final String RMBM = "RMB 11";
-
-//	@Test
-//	void test() {
-//		fail("Not yet implemented");
-//	}
+	// millionPostNames.add(" million yuan");
+	// billionPostNames.add(" billion yuan");
+	// trillionPostNames.add(" trillion yuan");
 
 	@Test
 	void testStart() {
 
 		// This is the basic test
 		int offset = 0;
-		Indicator indicator = new RMBIndicator(RMBA, "money");
+		Indicator indicator = new YuanIndicator("com.debugger.Yuan", "money");
+	
+		int lenghtOfValue = MONTHA.length();
+		Collection<Annotation> annotations = indicator.identify(MONTHA+END_OF_SENTENCE, offset);
+		assertTrue(annotations.size()==1);
+		Annotation annotation = annotations.iterator().next(); 
+		assertTrue(MONTHA_VALUE.equals(annotation.value));
+		assertTrue(annotation.start == (0+offset));
+		assertTrue(annotation.stop == (MONTHA.length())); 
+		assertTrue(annotation.tags.contains("money"));
 
-		int lenghtOfValue = RMBA.length();
-		Collection<Annotation> annotations = indicator.identify(RMBA + END_OF_SENTENCE, offset);
-		assertTrue(annotations.size() == 1);
-		Annotation annotation = annotations.iterator().next();
-		assertTrue("88.14".equals(annotation.value));
-		assertTrue(annotation.start == (0 + offset));
-		assertTrue(annotation.stop == (RMBA.length()));
+		lenghtOfValue = MONTHB.length();
+		annotations = indicator.identify(MONTHB+END_OF_SENTENCE, offset);
+		assertTrue(annotations.size()==1);
+		annotation = annotations.iterator().next(); 
+		assertTrue(MONTHB_VALUE.equals(annotation.value));
+		assertTrue(annotation.start == (0+offset));
+		assertTrue(annotation.stop == (MONTHB.length())); 
 		assertTrue(annotation.tags.contains("money"));
-		assertTrue(annotation.tags.contains("billion"));
-
-		lenghtOfValue = RMBB.length();
-		annotations = indicator.identify(RMBB + END_OF_SENTENCE, offset);
-		assertTrue(annotations.size() == 1);
-		annotation = annotations.iterator().next();
-		assertTrue("13".equals(annotation.value));
-		assertTrue(annotation.start == (0 + offset));
-		assertTrue(annotation.stop == (RMBB.length()));
+		
+		lenghtOfValue = MONTHC.length();
+		annotations = indicator.identify(MONTHC+END_OF_SENTENCE, offset);
+		assertTrue(annotations.size()==1);
+		annotation = annotations.iterator().next(); 
+		assertTrue(MONTHC_VALUE.equals(annotation.value));
+		assertTrue(annotation.start == (0+offset));
+		assertTrue(annotation.stop == (MONTHC.length())); 
 		assertTrue(annotation.tags.contains("money"));
-		assertTrue(annotation.tags.contains("billion"));
-
-		lenghtOfValue = RMBC.length();
-		annotations = indicator.identify(RMBC + END_OF_SENTENCE, offset);
-		assertTrue(annotations.size() == 1);
-		annotation = annotations.iterator().next();
-		assertTrue("55".equals(annotation.value));
-		assertTrue(annotation.start == (0 + offset));
-		assertTrue(annotation.stop == (RMBC.length()));
-		assertTrue(annotation.tags.contains("money"));
-		assertTrue(annotation.tags.contains("billion"));
-
-		lenghtOfValue = RMBD.length();
-		annotations = indicator.identify(RMBD + END_OF_SENTENCE, offset);
-		assertTrue(annotations.size() == 1);
-		annotation = annotations.iterator().next();
-		assertTrue("21.9".equals(annotation.value));
-		assertTrue(annotation.start == (0 + offset));
-		assertTrue(annotation.stop == lenghtOfValue);
-		assertTrue(annotation.tags.contains("money"));
-		assertTrue(annotation.tags.contains("billion"));
-
-		lenghtOfValue = RMBE.length();
-		annotations = indicator.identify(RMBE + END_OF_SENTENCE, offset);
-		assertTrue(annotations.size() == 1);
-		annotation = annotations.iterator().next();
-		assertTrue("141".equals(annotation.value));
-		assertTrue(annotation.start == (0 + offset));
-		assertTrue(annotation.stop == lenghtOfValue);
-		assertTrue(annotation.tags.contains("money"));
-		assertTrue(annotation.tags.contains("billion"));
-
-		lenghtOfValue = RMBF.length();
-		annotations = indicator.identify(RMBF + END_OF_SENTENCE, offset);
-		assertTrue(annotations.size() == 1);
-		annotation = annotations.iterator().next();
-		assertTrue("141".equals(annotation.value));
-		assertTrue(annotation.start == (0 + offset));
-		assertTrue(annotation.stop == lenghtOfValue);
-		assertTrue(annotation.tags.contains("money"));
-		assertTrue(annotation.tags.contains("billion"));
-
-		lenghtOfValue = RMBG.length();
-		annotations = indicator.identify(RMBG + END_OF_SENTENCE, offset);
-		assertTrue(annotations.size() == 1);
-		annotation = annotations.iterator().next();
-		assertTrue("1.9".equals(annotation.value));
-		assertTrue(annotation.start == (0 + offset));
-		assertTrue(annotation.stop == lenghtOfValue);
-		assertTrue(annotation.tags.contains("money"));
-		assertTrue(annotation.tags.contains("billion"));
-
-		lenghtOfValue = RMBH.length();
-		annotations = indicator.identify(RMBH + END_OF_SENTENCE, offset);
-		assertTrue(annotations.size() == 1);
-		annotation = annotations.iterator().next();
-		assertTrue("25.5".equals(annotation.value));
-		assertTrue(annotation.start == (0 + offset));
-		assertTrue(annotation.stop == lenghtOfValue);
-		assertTrue(annotation.tags.contains("money"));
-		assertTrue(annotation.tags.contains("billion"));
-
-		lenghtOfValue = RMBI.length();
-		annotations = indicator.identify(RMBI + END_OF_SENTENCE, offset);
-		assertTrue(annotations.size() == 2);
-		annotation = annotations.iterator().next();
-		assertTrue("3.5".equals(annotation.value) || ("4.5".equals(annotation.value)));
-		assertTrue(annotation.start == (0 + offset));
-		assertTrue(annotation.stop == lenghtOfValue);
-		assertTrue(annotation.tags.contains("money"));
-		assertTrue(annotation.tags.contains("billion"));
-		// we want two
-		annotation = annotations.iterator().next();
-		assertTrue("3.5".equals(annotation.value) || ("4.5".equals(annotation.value)));
-		assertTrue(annotation.start == (0 + offset));
-		assertTrue(annotation.stop == lenghtOfValue);
-		assertTrue(annotation.tags.contains("money"));
-		assertTrue(annotation.tags.contains("billion"));
-
-		lenghtOfValue = RMBJ.length();
-		annotations = indicator.identify(RMBJ + END_OF_SENTENCE, offset);
-		assertTrue(annotations.size() == 2);
-		annotation = annotations.iterator().next();
-		assertTrue("7".equals(annotation.value) || ("12".equals(annotation.value)));
-		assertTrue(annotation.start == (0 + offset));
-		assertTrue(annotation.stop == lenghtOfValue);
-		assertTrue(annotation.tags.contains("money"));
-		assertTrue(annotation.tags.contains("billion"));
-		// we want two
-		annotation = annotations.iterator().next();
-		assertTrue("7".equals(annotation.value) || ("12".equals(annotation.value)));
-		assertTrue(annotation.start == (0 + offset));
-		assertTrue(annotation.stop == lenghtOfValue);
-		assertTrue(annotation.tags.contains("money"));
-		assertTrue(annotation.tags.contains("billion"));
+		
 	}
+	
+	
+	@Test
+	void testEnd() {
+		
+		// (START_OF_SENTENCE+" ")
+				int offset = 50;
+				Indicator indicator = new YuanIndicator("com.debugger.Yuan", "money");
+			
+				int lenghtOfValue = MONTHA.length();
+				Collection<Annotation> annotations = indicator.identify((START_OF_SENTENCE+" ")+MONTHA, offset);
+				assertTrue(annotations.size()==1);
+				Annotation annotation = annotations.iterator().next(); 
+				assertTrue(MONTHA_VALUE.equals(annotation.value));
+				assertTrue(annotation.start == ((START_OF_SENTENCE+" ").length()+offset));
+				assertTrue(annotation.stop == ((START_OF_SENTENCE+" ").length()+offset)+MONTHA.length()); 
+				assertTrue(annotation.tags.contains("money"));
 
-	/*
-	 * 
-	 * 
-	 * 
-	 * 9,504 billion dollars 13.45 billion U.S. dollars 200 billion U.S. dollars
-	 * US$100 billion 3 billion dollars $66.1 billion US 1.9 billion dollars 25.5
-	 * Billion US Dollars $3.5–$4.5 billion $7-$12 billion
-	 */
+				lenghtOfValue = MONTHB.length();
+				annotations = indicator.identify((START_OF_SENTENCE+" ")+MONTHB, offset);
+				assertTrue(annotations.size()==1);
+				annotation = annotations.iterator().next(); 
+				assertTrue(MONTHB_VALUE.equals(annotation.value));
+				assertTrue(annotation.start == ((START_OF_SENTENCE+" ").length()+offset));
+				assertTrue(annotation.stop == ((START_OF_SENTENCE+" ").length()+offset)+MONTHB.length()); 
+				assertTrue(annotation.tags.contains("money"));
+				
+				lenghtOfValue = MONTHC.length();
+				annotations = indicator.identify((START_OF_SENTENCE+" ")+MONTHC, offset);
+				assertTrue(annotations.size()==1);
+				annotation = annotations.iterator().next(); 
+				assertTrue(MONTHC_VALUE.equals(annotation.value));
+				assertTrue(annotation.start == ((START_OF_SENTENCE+" ").length()+offset));
+				assertTrue(annotation.stop == ((START_OF_SENTENCE+" ").length()+offset)+MONTHC.length()); 
+				assertTrue(annotation.tags.contains("money"));
+
+	}
+	
+	
 }
